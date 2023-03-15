aabc4dc2c5f4285ffa60ba810859a1b696210e34
UrielCh <uriel.chemouni@gmail.com>
2023-03-15 07:45:42 +0200

    add exportMat

--- cc/module.cc
+++ cc/module.cc
@@ -21,6 +21,35 @@ Napi::Number MultiplyByTwo(const Napi::CallbackInfo &info)
     return Napi::Number::New(env, result);
 }
 
+
+
+Napi::Value exportMat(Napi::Env &env, const cv::Mat& img)
+{
+    Napi::Object obj = Napi::Object::New(env);
+
+    size_t size = img.total() * img.elemSize();
+    Napi::Buffer<uint8_t> buffer = Napi::Buffer<uint8_t>::New(env, img.data, size);
+    obj.Set(Napi::String::New(env, "data"), buffer);
+    
+    // auto matPtr = &img;
+    // Napi::External<cv::Mat> external = Napi::External<cv::Mat>::New(env, matPtr, [](Napi::Env env, cv::Mat *data)
+    //                                                                 { delete data; });
+
+    obj.Set(Napi::String::New(env, "dims"), Napi::Number::New(env, img.dims));
+    obj.Set(Napi::String::New(env, "rows"), Napi::Number::New(env, img.rows));
+    obj.Set(Napi::String::New(env, "cols"), Napi::Number::New(env, img.cols));
+
+    // contains type, depth, channels
+    obj.Set(Napi::String::New(env, "flags"), Napi::Number::New(env, img.flags));
+    // flags as exploded data view
+    obj.Set(Napi::String::New(env, "channels"), Napi::Number::New(env, img.channels()));
+    obj.Set(Napi::String::New(env, "depth"), Napi::Number::New(env, img.depth()));
+    obj.Set(Napi::String::New(env, "type"), Napi::Number::New(env, img.type()));
+
+    return obj;
+}
+
+
 Napi::Value AllocateMat(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
@@ -42,16 +71,17 @@ Napi::Value AllocateMat(const Napi::CallbackInfo &info)
     }
 
     cv::Mat *mat = new cv::Mat(rows, cols, type);
-    Napi::External<cv::Mat> external = Napi::External<cv::Mat>::New(env, mat, [](Napi::Env env, cv::Mat *data)
-                                                                    { delete data; });
+    return exportMat(env, *mat);
 
-    // return
-    Napi::Object result = Napi::Object::New(env);
-    result.Set(Napi::String::New(env, "data"), external);
-    result.Set(Napi::String::New(env, "rows"), rows);
-    result.Set(Napi::String::New(env, "cols"), cols);
-    result.Set(Napi::String::New(env, "type"), type);
-    return result;
+    //Napi::External<cv::Mat> external = Napi::External<cv::Mat>::New(env, mat, [](Napi::Env env, cv::Mat *data)
+    //                                                                { delete data; });
+    //// return
+    //Napi::Object result = Napi::Object::New(env);
+    //result.Set(Napi::String::New(env, "data"), external);
+    //result.Set(Napi::String::New(env, "rows"), rows);
+    //result.Set(Napi::String::New(env, "cols"), cols);
+    //result.Set(Napi::String::New(env, "type"), type);
+    //return result;
 }
 
 Napi::Value ReadImage(const Napi::CallbackInfo &info)
@@ -73,16 +103,51 @@ Napi::Value ReadImage(const Napi::CallbackInfo &info)
         return env.Null();
     }
 
-    size_t size = img.total() * img.elemSize();
-    Napi::Buffer<uint8_t> buffer = Napi::Buffer<uint8_t>::New(env, img.data, size);
-    Napi::Object obj = Napi::Object::New(env);
-    obj.Set(Napi::String::New(env, "data"), buffer);
-    obj.Set(Napi::String::New(env, "rows"), Napi::Number::New(env, img.rows));
-    obj.Set(Napi::String::New(env, "cols"), Napi::Number::New(env, img.cols));
-    obj.Set(Napi::String::New(env, "channels"), Napi::Number::New(env, img.channels()));
-
-    return obj;
+    return exportMat(env, img);
+    // Napi::Object obj = Napi::Object::New(env);
+    // size_t size = img.total() * img.elemSize();
+    // Napi::Buffer<uint8_t> buffer = Napi::Buffer<uint8_t>::New(env, img.data, size);
+    // obj.Set(Napi::String::New(env, "data"), buffer);
+    // obj.Set(Napi::String::New(env, "flags"), Napi::Number::New(env, img.flags)); // contains type, depth, channels
+    // obj.Set(Napi::String::New(env, "dims"), Napi::Number::New(env, img.dims));
+    // obj.Set(Napi::String::New(env, "rows"), Napi::Number::New(env, img.rows));
+    // obj.Set(Napi::String::New(env, "cols"), Napi::Number::New(env, img.cols));
+    // obj.Set(Napi::String::New(env, "channels"), Napi::Number::New(env, img.channels()));
+    // return obj;
 }
+
+
+// #define ERRWRAP2
+
+
+// replace "static PyObject*" by Napi::Value
+// replace "PyObject* , PyObject* py_args, PyObject* kw" by const Napi::CallbackInfo &info
+// Napi::Value pyopencv_cv_imread(const Napi::CallbackInfo &info)
+// {
+//     // drop 
+//     // using namespace cv;
+// 
+//     // PyObject* pyobj_filename = NULL;
+//     std::string pyobj_filename = NULL;
+// 
+//     String filename;
+//     PyObject* pyobj_flags = NULL;
+//     int flags=IMREAD_COLOR;
+//     Mat retval;
+// 
+//     const char* keywords[] = { "filename", "flags", NULL };
+//     if( PyArg_ParseTupleAndKeywords(py_args, kw, "O|O:imread", (char**)keywords, &pyobj_filename, &pyobj_flags) &&
+//         pyopencv_to_safe(pyobj_filename, filename, ArgInfo("filename", 0)) &&
+//         pyopencv_to_safe(pyobj_flags, flags, ArgInfo("flags", 0)) )
+//     {
+//         ERRWRAP2(retval = cv::imread(filename, flags));
+//         return pyopencv_from(retval);
+//     }
+// 
+//     return NULL;
+// }
+
+
 
 Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
