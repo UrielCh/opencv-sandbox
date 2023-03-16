55c3f869adfa510a58e38aad55eae787b39e9fd1
UrielCh <uriel.chemouni@gmail.com>
2023-03-16 08:00:34 +0200

    add some dirty code.

---
+++ cc/parse.cc
@@ -0,0 +1,95 @@
+/**
+ * CharGPT 3.5 based parser.
+ */
+#include <napi.h>
+#include <vector>
+#include <unordered_map>
+#include <cstring>
+#include <stdarg.h>
+
+template<typename T>
+T parse_napi_value(const Napi::Env& env, const napi_value& value) {
+    if (!value.IsNumber() && !value.IsBoolean()) {
+        throw std::invalid_argument("Invalid argument type: expected number or boolean");
+    }
+    return static_cast<T>(Napi::Value(env, value).ToNumber());
+}
+
+void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
+                                std::vector<napi_value>& args,
+                                std::unordered_map<std::string, napi_value>& kwargs) {
+    napi_env env = info.Env();
+
+    size_t argc = info.Length();
+
+    // parse positional arguments
+    for (size_t i = 0; i < argc; i++) {
+        args.push_back(info[i]);
+    }
+
+    // parse keyword arguments
+    if (argc > 0 && info[argc - 1].IsObject()) {
+        Napi::Object kwargs_obj = info[argc - 1].As<Napi::Object>();
+        std::vector<napi_value> keys = kwargs_obj.GetPropertyNames();
+
+        for (napi_value& key : keys) {
+            std::string key_str = Napi::Value(env, key).ToString().Utf8Value();
+            kwargs[key_str] = kwargs_obj.Get(key);
+        }
+    }
+}
+
+
+int NodeArg_ParseTupleAndKeywords(const Napi::CallbackInfo& info, const char *format, char **kwlist, ...) {
+    // initialize the list of output parameters
+    va_list args;
+    va_start(args, kwlist);
+
+    // parse the input arguments into a vector of napi_values
+    std::vector<napi_value> args_list;
+    std::unordered_map<std::string, napi_value> kwargs_map;
+    parse_napi_args_and_kwargs(info, args_list, kwargs_map);
+
+    // parse the format string and keyword list
+    size_t num_args = args_list.size();
+    size_t num_kwlist = sizeof(kwlist) / sizeof(kwlist[0]);
+    size_t i = 0, j = 0;
+
+    // iterate over the format string
+    while (format[i] != '\0') {
+        if (format[i] == '|') {
+            // ignore vertical bars in format string
+            i++;
+        } else if (format[i] == ':') {
+            // parse keyword arguments
+            const char* kwname = kwlist[j++];
+            if (kwargs_map.count(kwname) > 0) {
+                // if keyword argument is present, parse it using the format specifier
+                const napi_value& arg = kwargs_map[kwname];
+                parse_napi_value(arg, format[i+1], args, args_list, j-1);
+            } else {
+                // if keyword argument is not present, set its value to NULL or default value
+                const char* defvalue = kwlist[j++];
+                if (defvalue == nullptr) {
+                    va_arg(args, void*);
+                } else {
+                    parse_default_value(defvalue, format[i+1], args);
+                }
+            }
+            i += 2;
+        } else {
+            // parse positional arguments
+            if (i >= num_args) {
+                // if there are not enough arguments, raise an error
+                Napi::Error::New(info.Env(), "Not enough arguments").ThrowAsJavaScriptException();
+                return 0;
+            }
+            const napi_value& arg = args_list[i++];
+            parse_napi_value(arg, format[i], args, args_list, i-1);
+            i++;
+        }
+    }
+
+    va_end(args);
+    return 1;
+}
