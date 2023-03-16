fa3784b2dd60a1ea32baa796c0c6b598d0ccd070
UrielCh <uriel.chemouni@gmail.com>
2023-03-16 09:35:46 +0200

    add comments

--- cc/parse.cc
+++ cc/parse.cc
@@ -7,6 +7,9 @@
 #include <cstring>
 #include <stdarg.h>
 
+// see doc: https://github.com/nodejs/node-addon-api/blob/main/doc/value.md
+
+
 template<typename T>
 T parse_napi_value(const Napi::Env& env, const napi_value& value) {
     if (!value.IsNumber() && !value.IsBoolean()) {
@@ -15,6 +18,19 @@ T parse_napi_value(const Napi::Env& env, const napi_value& value) {
     return static_cast<T>(Napi::Value(env, value).ToNumber());
 }
 
+/**
+ * Parse the arguments and keyword arguments passed to a N-API callback function,
+ * and store them in two data structures: a vector of positional arguments and an
+ * unordered map of keyword arguments. If the last argument passed to the function
+ * is an object, it is interpreted as a dictionary of keyword arguments. The keys
+ * of the dictionary are the argument names, and the values are the argument values.
+ * If any argument is an array, it is converted to a vector of napi_values before being
+ * stored in the positional arguments vector.
+ * @param info The callback information object for the N-API function.
+ * @param args [output] A vector to store the positional arguments of the function.
+ * @param kwargs [output] An unordered map to store the keyword arguments of the function.
+ * @throws std::invalid_argument if an argument is not a number or boolean.
+*/
 void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
                                 std::vector<napi_value>& args,
                                 std::unordered_map<std::string, napi_value>& kwargs) {
@@ -26,6 +42,9 @@ void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
     for (size_t i = 0; i < argc; i++) {
         napi_value arg = info[i];
         Napi::Value arg_value(env, arg);
+        // Python style detect if the parameters is an array ???
+        // not relevant for our case.  ???
+        // will flatten all parameters, not that we need. ???
         if (arg_value.IsArray()) {
             // convert array to vector
             Napi::Array arg_array = arg_value.As<Napi::Array>();
@@ -34,6 +53,7 @@ void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
                 napi_value element = arg_array.Get(j);
                 arg_vector.push_back(element);
             }
+            // insert flaten parameter at the end of the current args ???
             args.insert(args.end(), arg_vector.begin(), arg_vector.end());
         } else {
             args.push_back(arg);
@@ -41,12 +61,19 @@ void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
     }
 
     // parse keyword arguments
+    // if the last pargument id an object it is interpreted as a dictionary of keyword arguments ???
+    // that one looks correct. but can cause issue is the last param is a Mat ???
+    // .IsObject() is like typeof A === "object" ???
     if (argc > 0 && info[argc - 1].IsObject()) {
         Napi::Object kwargs_obj = info[argc - 1].As<Napi::Object>();
         // std::vector<Napi::Value>
+        // GetPropertyNames: The properties whose key is a `Symbol` will not be included. ???
         Napi::Array keys = kwargs_obj.GetPropertyNames();
+        // must be an array ???
+        // should thows an Error if not an array ???
         if (keys.IsArray()) {
             Napi::Array arg_array = keys.As<Napi::Array>();
+            // get all the keys of the object convert them as Napi::String then use the key to get the napi_value ???
             for (size_t j = 0; j < arg_array.Length(); j++) {
                 Napi::String key_str = arg_array.Get(j).As<Napi::String>();
                 kwargs[key_str] = kwargs_obj.Get(key_str);
@@ -56,7 +83,17 @@ void parse_napi_args_and_kwargs(const Napi::CallbackInfo& info,
     }
 }
 
+/**
+ * Take info as input to extract parameters to be passed to the C++ function
+ * 
+ * @param info nAPI callback info
+ * @param format format string as as pyhon format like "OOO|OO:fncname"
+ * @param kwlist parameter names
+ * @param ... output parameters
+ */
 int NodeArg_ParseTupleAndKeywords(const Napi::CallbackInfo& info, const char *format, char **kwlist, ...) {
+    Napi::Env env = info.Env();
+
     // initialize the list of output parameters
     va_list args;
     va_start(args, kwlist);
@@ -101,7 +138,7 @@ int NodeArg_ParseTupleAndKeywords(const Napi::CallbackInfo& info, const char *fo
                 return 0;
             }
             const napi_value& arg = args_list[i++];
-            parse_napi_value(arg, format[i], args, args_list, i-1);
+            parse_napi_value(env, arg, format[i], args);
             i++;
         }
     }
