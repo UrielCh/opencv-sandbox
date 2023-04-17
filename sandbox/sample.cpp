bddf4ab845acbc63cbeafaed24671c68b0de4b41
UrielCh <uriel.chemouni@gmail.com>
2023-04-17 12:43:16 +0300

    add Napi::Symbol im sample.cpp

--- sandbox/sample.cpp
+++ sandbox/sample.cpp
@@ -19,11 +19,13 @@ public:
     }
 };
 
+Napi::Symbol AKAZE_KEY;
+
 void jsopencv_cv_AKAZE_setThreshold(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     double threshold = info[0].As<Napi::Number>().DoubleValue();
-    AKAZE *akaze = info.This().As<Napi::Object>().GetInternalField(0).As<Napi::External<AKAZE>>().Data();
+    AKAZE *akaze = info.This().As<Napi::Object>().Get(AKAZE_KEY).As<Napi::External<AKAZE>>().Data();
     akaze->setThreshold(threshold);
 }
 
@@ -31,7 +33,7 @@ void jsopencv_cv_AKAZE_setDescriptorSize(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     int descriptorSize = info[0].As<Napi::Number>().Int32Value();
-    AKAZE *akaze = info.This().As<Napi::Object>().GetInternalField(0).As<Napi::External<AKAZE>>().Data();
+    AKAZE *akaze = info.This().As<Napi::Object>().Get(AKAZE_KEY).As<Napi::External<AKAZE>>().Data();
     akaze->setDescriptorSize(descriptorSize);
 }
 
@@ -39,7 +41,7 @@ void jsopencv_cv_AKAZE_setDescriptorChannels(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     int descriptorChannels = info[0].As<Napi::Number>().Int32Value();
-    AKAZE *akaze = info.This().As<Napi::Object>().GetInternalField(0).As<Napi::External<AKAZE>>().Data();
+    AKAZE *akaze = info.This().As<Napi::Object>().Get(AKAZE_KEY).As<Napi::External<AKAZE>>().Data();
     akaze->setDescriptorChannels(descriptorChannels);
 }
 
@@ -47,7 +49,7 @@ void jsopencv_cv_AKAZE_setDescriptorType(const Napi::CallbackInfo &info)
 {
     Napi::Env env = info.Env();
     int descriptorType = info[0].As<Napi::Number>().Int32Value();
-    AKAZE *akaze = info.This().As<Napi::Object>().GetInternalField(0).As<Napi::External<AKAZE>>().Data();
+    AKAZE *akaze = info.This().As<Napi::Object>().Get(AKAZE_KEY).As<Napi::External<AKAZE>>().Data();
     akaze->setDescriptorType(descriptorType);
 }
 
@@ -55,11 +57,13 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
 {
     Napi::HandleScope scope(env);
 
+    AKAZE_KEY = Napi::Symbol::New(env);
+
     Napi::Function akazeConstructor = Napi::Function::New(env, [](const Napi::CallbackInfo &info)
                                                           {
         Napi::Env env = info.Env();
         AKAZE *akaze = new AKAZE();
-        info.This().As<Napi::Object>().SetInternalField(0, Napi::External<AKAZE>::New(env, akaze));
+        info.This().As<Napi::Object>().Set(AKAZE_KEY, Napi::External<AKAZE>::New(env, akaze));
         return info.This(); });
 
     Napi::Function setThreshold = Napi::Function::New(env, jsopencv_cv_AKAZE_setThreshold);
@@ -72,7 +76,7 @@ Napi::Object Init(Napi::Env env, Napi::Object exports)
         Napi::Env env = info.Env();
         AKAZE akaze = AKAZE::getDefault();
         Napi::Object obj = Napi::Object::New(env);
-        obj.SetInternalField(0, Napi::External<AKAZE>::New(env, new AKAZE(akaze)));
+        obj.Set(AKAZE_KEY, Napi::External<AKAZE>::New(env, new AKAZE(akaze)));
         return obj; });
     akazeConstructor["getDefault"] = getDefault;
     akazeConstructor.As<Napi::Object>().DefineOwnProperty(env, Napi::String::New(env, "setThreshold"), setThreshold);
